1cc06c44de0b54601e207c7048f1048b4e72832c
RyanEka25 <octavianoryan030@gmail.com>
2023-07-03 19:26:04 +0700

    Commit kedua

--- database/db.go
+++ database/db.go
@@ -4,23 +4,35 @@ import (
 	"OctavianoRyan25/GO-JWT/model"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-	host     = "localhost"
-	port     = "5432"
-	user     = "postgres"
-	password = "mulianet010"
-	dbname   = "api"
+	// host     = "localhost"
+	// port     = "5432"
+	// user     = "postgres"
+	// password = "mulianet010"
+	// dbname   = "api"
+	
 	db       *gorm.DB
-	err      error
 )
 
 func StartDB() {
-	config := fmt.Sprintf("host = %s user = %v password = %v dbname = %v port = %v sslmode = disable", host, user, password, dbname, port)
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Gagal memuat file .env")
+    }
+	
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbPass := os.Getenv("DB_PASS")
+    dbName := os.Getenv("DB_NAME")
+	config := fmt.Sprintf("host = %s user = %v password = %v dbname = %v port = %v sslmode = disable", dbHost, dbUser, dbPass, dbName, dbPort)
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 	if err != nil {
